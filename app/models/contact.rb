1cef850c9a3c48c788662b58ede37eb0633ab242
Doug Niccum <dniccum@ingenology.com>
2015-07-05 15:14:56 -0500

    Adds scope lambda model methods

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,13 @@
 class Contact < ActiveRecord::Base
     belongs_to :user
+
+    scope :allContacts, lambda { |user|
+        where(:user_id => "#{user}")
+    }
+    scope :byLastName, lambda { order("contacts.last_name ASC") }
+    scope :byFirstName, lambda { order("contacts.first_name ASC") }
+    scope :byCompany, lambda { order("contacts.company ASC") }
+    scope :search, lambda { |query|
+        where(["first_name like ?", "%#{query}%"])
+    }
 end
