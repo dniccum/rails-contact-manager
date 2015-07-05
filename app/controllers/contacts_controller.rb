6fde479fd5ed5b5c4fae9730a753399d01e8d820
Doug Niccum <dniccum@ingenology.com>
2015-07-05 14:00:11 -0500

    Adds methods to create a contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,11 +7,27 @@ class ContactsController < ApplicationController
     end
 
     def show_create
+        @contact = Contact.new
+
         render('create')
     end
 
     def create
+        @contact = Contact.new(contact_params)
 
+        if @contact.save
+            flash[:success] = "The contact '#{@contact.first_name} #{@contact.last_name}' has been created."
+            redirect_to(:action => 'index')
+        else
+            flash[:notice] = "Please complete all fields."
+            render('create')
+        end
     end
-    
+
+    private
+
+        def contact_params
+            params.require(:contact).permit(:first_name, :last_name, :email, :company, :notes, :user_id)
+        end
+
 end
