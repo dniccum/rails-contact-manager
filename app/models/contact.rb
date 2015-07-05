9aa7ec07ffb2b45b5c4e2a1f7227d91c16a054c3
Doug Niccum <dniccum@ingenology.com>
2015-07-05 17:28:49 -0500

    Adds validation to both models

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,11 @@
 class Contact < ActiveRecord::Base
     belongs_to :user
 
+    validates :first_name, presence: true, length: { maximum: 50 }
+    validates :last_name, presence: true, length: { maximum: 50 }
+    validates :email, presence: true, length: { maximum: 50 }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+    validates :company, length: { maximum: 50 }
+
     scope :allContacts, lambda { |user|
         where(:user_id => "#{user}")
     }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
     has_secure_password
 
+    validates :first_name, presence: true, length: { maximum: 50 }
+    validates :last_name, presence: true, length: { maximum: 50 }
+    validates :email, uniqueness: true, presence: true, length: { maximum: 50 }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+    validates :password, length: 6..20
+
     has_many :contacts
 end
