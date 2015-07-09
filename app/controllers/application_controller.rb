9d9395ffe52862373c1512186b9e890e2b75a408
Doug Niccum <dniccum@ingenology.com>
2015-07-08 22:54:15 -0500

    Adds additional logic to test for the auth_token cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,18 @@ class ApplicationController < ActionController::Base
   private
 
   def confirm_sign_in
-      unless session[:user_id]
-          flash[:notice] = 'Please log in.'
-          redirect_to(:controller => 'auth', :action => 'get_sign_in')
-          return false
+      if cookies[:auth_token]
+          user = User.find_by_auth_token!(cookies[:auth_token])
+          session[:user_id] = user.id
+          session[:email] = user.email
       else
-          return true
+          unless session[:user_id]
+              flash[:notice] = 'Please log in.'
+              redirect_to(:controller => 'auth', :action => 'get_sign_in')
+              return false
+          else
+              return true
+          end
       end
   end
 end
