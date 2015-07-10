5adeaa6d3d79dff37a1ea8ef905140f8154eb06d
Doug Niccum <dniccum@ingenology.com>
2015-07-09 23:04:04 -0500

    Removes old validations that are now covered by devise and removes old before_create method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,12 @@
 class User < ActiveRecord::Base
-    before_create { generate_token(:auth_token) }
-
-    has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
     validates :first_name, presence: true, length: { maximum: 50 }
     validates :last_name, presence: true, length: { maximum: 50 }
-    validates :email, uniqueness: true, presence: true, length: { maximum: 50 }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-    validates :password, length: 6..20
 
     has_many :contacts
 
-    def send_password_reset
-        generate_token(:password_reset_token)
-        self.password_reset_sent_at = Time.zone.now
-        save!(:validate => false)
-        UserMailer.password_reset(self).deliver
-    end
-
-    def generate_token(column)
-        begin
-            self[column] = SecureRandom.urlsafe_base64
-        end while User.exists?(column => self[column])
-    end
 end
