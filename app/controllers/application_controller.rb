85961d972cdaa0bc2c98ff5dfc0c029f61cac05c
Doug Niccum <dniccum@ingenology.com>
2015-07-09 22:56:35 -0500

    Removes old private signin tests and defines a custom sign in path for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,21 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
-
-  def confirm_sign_in
-      if cookies[:auth_token]
-          user = User.find_by_auth_token!(cookies[:auth_token])
-          session[:user_id] = user.id
-          session[:email] = user.email
-      else
-          unless session[:user_id]
-              flash[:notice] = 'Please log in.'
-              redirect_to(:controller => 'auth', :action => 'get_sign_in')
-              return false
-          else
-              return true
-          end
-      end
+  def after_sign_in_path_for(user)
+      '/contacts'
   end
+
 end
